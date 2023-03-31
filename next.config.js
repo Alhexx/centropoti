de2e3506c01b1701f65e5624c6ecab4f8fa96653
Alexandre Cesar <guimaraes.aco@gmail.com>
2023-03-31 12:19:37 -0300

    testing images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+/** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   basePath: "",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+    };
+  },
+  // withPlugins: [
+  //   [
+  //     optimizedImages,
+  //     {
+  //       /* config for next-optimized-images */
+  //     },
+  //   ],
+
+  //   // your other plugins here
+  // ],
+  images: {
+    unoptimized: true,
+  },
 };
